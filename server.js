5a76bb63cbe5bfa894fda687b1e31eb6cfada0ed
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-02-18 18:42:31 +0200

    Added ./db

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
 
 const port = process.env.PORT || 5000;
-connectDB();
 app.listen(port);
 
 console.log(`Server is running on port ${port}`);
