c9bc10634b9fd6aa65af43503495376688ddd48f
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-02-18 18:34:38 +0200

    Added DB()

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Mongoose = require("mongoose")
-const localDB = `mongodb+srv://tsymbaljuk2001:<password>@cluster0.k870rvy.mongodb.net/?retryWrites=true&w=majority`
+const localDB = `mongodb+srv://tsymbaljuk2001:messi10ronaldo7@cluster0.k870rvy.mongodb.net/?retryWrites=true&w=majority`
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
     useNewUrlParser: true,
