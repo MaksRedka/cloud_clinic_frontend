e1271afcbe7a2e7ce7d38d50b2c7d34a78113616
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-02-18 18:22:39 +0200

    Added DB()

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Mongoose = require("mongoose")
-const localDB = `mongodb://localhost:27017/role_auth`
+const localDB = `mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority`
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
     useNewUrlParser: true,
