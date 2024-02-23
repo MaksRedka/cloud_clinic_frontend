2b2185f2c2ba651e83c13b62fe8ebc068f619b76
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-02-23 11:47:00 +0200

    Pushing to heroku

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const app = express();
+const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+
+// getting the local authentication type
+const LocalStrategy = require('passport-local').Strategy
 
 app.use(serveStatic(path.join(__dirname, "dist")));
 
