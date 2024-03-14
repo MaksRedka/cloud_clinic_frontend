d092bbda4113393cb48f8e5328db75ea0f0d9177
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-03-14 12:47:17 +0200

    Trying to add database

--- src/layouts/account/MyCard.vue
+++ src/layouts/account/MyCard.vue
@@ -101,7 +101,29 @@ import {mdiMagnify} from "@mdi/js";
 
 
 console.log('MYCARD.vue');
-const db = require('../../../db');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://tsymbaljuk2001:messi10ronaldo7@cluster0.k870rvy.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+let db = run().catch(console.dir);
 console.log('db is here: ', db);
 
 const userData = ref({
