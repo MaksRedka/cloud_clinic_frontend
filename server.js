b3d3a74b10e1459eaf9cffe23174d037a344dd2a
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-03-14 20:02:42 +0200

    Connect DB

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const passport = require('passport')
 
 // getting the local authentication type
 const LocalStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose');
 
 app.use(serveStatic(path.join(__dirname, "dist")));
 
 const port = process.env.PORT || 5000;
+const uri = "mongodb+srv://tsymbaljuk2001:messi10ronaldo7@cluster0.k870rvy.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useNewUrlParser: true }).then(db => console.log('[OK] DB is connected')).catch(err => console.error(err));
 
 app.listen(port, function () {
     console.log(`Server listens http://cloud-clinic-123-7f92486a9893.herokuapp.com:${port}`);
