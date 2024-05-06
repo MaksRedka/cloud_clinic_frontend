9749ec61b315643b9001ea3956f84eb8e2fdd657
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-05-06 21:10:46 +0300

    Reverted app2

--- src/layouts/account/MyCard.vue
+++ src/layouts/account/MyCard.vue
@@ -88,6 +88,9 @@
             <div class="d-flex justify-center">
               <v-expansion-panel-text><strong>Активних планів немає</strong></v-expansion-panel-text>
             </div>
+            <div id="dwv">
+            <div id="layerGroup0"></div>
+            </div>
           </v-expansion-panel>
         </v-expansion-panels>
       </v-col>
@@ -111,7 +114,14 @@ const userData = ref({
     gender: 'male'
   },
 });
-
+// create the dwv app
+var app = new dwv.App();
+// initialise with the id of the container div
+app.init({
+  dataViewConfigs: {'*': [{divId: 'layerGroup0'}]}
+});
+// load dicom data
+app.loadURLs(['https://raw.githubusercontent.com/ivmartel/dwv/master/tests/data/bbmri-53323851.dcm']);
 const isDoctor = ref(true)
 const panels = ref([])
 const editedData = ref({age:30, location: 'City', gender: 'male'});
