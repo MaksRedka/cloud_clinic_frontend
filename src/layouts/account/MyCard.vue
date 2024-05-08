0ccb016945171274294d076ec5976ac3bea2ae44
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-05-08 22:14:34 +0300

    Add

--- src/layouts/account/MyCard.vue
+++ src/layouts/account/MyCard.vue
@@ -165,10 +165,6 @@ const userSchema = new mongoose.Schema({
 const userModel = mongoose.model('User', userSchema);
 const Ivan = new userModel({ name: "Ivan", surname: "Pavlov", aftersurname: "Ivanovich", "date_birth": "19.04.2004", "age": "32"}).save();
 
-console.log(Ivan.name);
-
-await Ivan.save();
-
 const users = userModel.find();
 console.log('Users are : ', users);
 
