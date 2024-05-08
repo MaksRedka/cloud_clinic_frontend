a7e7205fab4d6cbb69ce686192d7cf01cb935f92
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-05-08 21:14:32 +0300

    Found

--- server.js
+++ server.js
@@ -15,16 +15,26 @@ app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 5000;
 const uri = "mongodb+srv://tsymbaljuk2001:messi10ronaldo7@cluster0.k870rvy.mongodb.net/?retryWrites=true&w=majority";
 
-db = mongoose.connect(uri, { useNewUrlParser: true }).catch(err => console.error(err));
+mongoose.connect(uri, { useNewUrlParser: true }).catch(err => console.error(err));
 
-myDB = db.db("myDB");
-const myColl = myDB.collection("patient_data");
-const doc = { name: "Ivan", surname: "Ivanov", aftersurname: "Ivanovich", gener: "male", birth: "15.04.1992", age: "32" };
-const result = myColl.insertOne(doc);
-console.log(
-   `A document was inserted with the _id: ${result.insertedId}`,
-);
+const userSchema = new mongoose.Schema({
+    name: String,
+    surname: String,
+    aftersurname: String,
+    date_birth: String,
+    age: Number
+  });
 
+const userModel = mongoose.model('User', userSchema);
+const Ivan = new userModel({ name: "Ivan", surname: "Pavlov", aftersurname: "Ivanovich", "date_birth": "19.04.2004", "age": "32"});
+
+console.log(Ivan.name);
+
+await Ivan.save();
+
+const users = await userModel.find();
+
+console.log('Users are : ', users);
 
 app.listen(port, function () {
     console.log(`Server listens http://cloud-clinic-123-7f92486a9893.herokuapp.com:${port}`);
